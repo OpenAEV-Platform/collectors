d890bcdcd45eec183b19c90e01ae7090406a8fab
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-01-15 14:32:51 +0100

    isort
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- atomic-red-team/atomic_red_team/openaev_atomic_red_team.py
+++ atomic-red-team/atomic_red_team/openaev_atomic_red_team.py
@@ -3,9 +3,10 @@ import re
 
 import requests
 import yaml
-from atomic_red_team.configuration.config_loader import ConfigLoader
 from pyoaev.configuration import Configuration
 from pyoaev.daemons import CollectorDaemon
+
+from atomic_red_team.configuration.config_loader import ConfigLoader
 
 ATOMIC_RED_TEAM_INDEX = "https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/Indexes/index.yaml"
 
