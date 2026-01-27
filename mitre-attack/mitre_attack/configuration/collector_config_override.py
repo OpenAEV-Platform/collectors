bac8c0dec6e462f49d02401ed1d14c57994e51a7
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-01-27 14:13:01 +0100

    CR to LF
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- mitre-attack/mitre_attack/configuration/collector_config_override.py
+++ mitre-attack/mitre_attack/configuration/collector_config_override.py
@@ -1,1 +1,23 @@
-from datetime import timedeltafrom pydantic import Fieldfrom pyoaev.configuration import ConfigLoaderCollectorclass CollectorConfigOverride(ConfigLoaderCollector):    id: str = Field(        default="openaev_mitre_attack",        description="Collector unique identifier",    )    name: str = Field(        default="MITRE ATT&CK",        description="Collector display name",    )    icon_filepath: str | None = Field(        default="mitre_attack/img/icon-mitre-attack.png",        description="Path to the icon file",    )    period: timedelta | None = Field(        default=timedelta(days=7),        description="Duration between two scheduled runs of the collector (ISO 8601 format).",    )
+from datetime import timedelta
+
+from pydantic import Field
+from pyoaev.configuration import ConfigLoaderCollector
+
+
+class CollectorConfigOverride(ConfigLoaderCollector):
+    id: str = Field(
+        default="openaev_mitre_attack",
+        description="Collector unique identifier",
+    )
+    name: str = Field(
+        default="MITRE ATT&CK",
+        description="Collector display name",
+    )
+    icon_filepath: str | None = Field(
+        default="mitre_attack/img/icon-mitre-attack.png",
+        description="Path to the icon file",
+    )
+    period: timedelta | None = Field(
+        default=timedelta(days=7),
+        description="Duration between two scheduled runs of the collector (ISO 8601 format).",
+    )
