d280d579b5c5b8067611284fa485074ddffabbba
Frédéric Basler <frederic.basler@filigran.io>
2026-04-30 17:17:19 +0200

    [collectors] Up linter

--- splunk-es/src/models/configs/collector_configs.py
+++ splunk-es/src/models/configs/collector_configs.py
@@ -35,8 +35,9 @@ class _ConfigLoaderOAEV(ConfigBaseSettings):
     tenant_id: UUID | None = Field(
         default=None,
         description="Identifier of the tenant within the OpenAEV platform. Used in multi-tenant environments to scope "
-                    "API requests and ensure data isolation between different tenants.",
+        "API requests and ensure data isolation between different tenants.",
     )
+
 
 class _ConfigLoaderCollector(ConfigBaseSettings):
     """Base collector configuration settings.
