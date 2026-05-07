22a31d48625371aade8958fd819fd5bd346874dc
guzmud <ferdinand.bellissime@filigran.io>
2026-05-07 11:51:19 +0200

    [template] feat(protocols): adding collector protocols to the template

--- template/src/collector/protocols/data_fetcher.py
+++ template/src/collector/protocols/data_fetcher.py
@@ -1,6 +1,8 @@
-from typing import Protocol
+from typing import Protocol, runtime_checkable
+
+from src.collector.protocols.source_data import SourceDataProtocol
 
 
+@runtime_checkable
 class DataFetcherProtocol(Protocol):
-    def fetch_data(self):
-        ...
+    def fetch_data(self) -> list[SourceDataProtocol]: ...
