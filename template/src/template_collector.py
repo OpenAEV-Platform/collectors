529b67e0b2ae826cbe80e48ef45f249a84364182
guzmud <ferdinand.bellissime@filigran.io>
2026-07-08 16:16:11 +0200

    fix(template): improving the logging in the main function (#462)

--- template/src/template_collector.py
+++ template/src/template_collector.py
@@ -4,6 +4,7 @@ based on the distinction between the normalized collector engine
 and the custom source related to the implemented tool/service
 """
 
+import logging
 import os
 import sys
 
@@ -19,6 +20,9 @@ def main() -> None:
     defining a source, feeding it into the base collector,
     then starting said collector
     """
+    logging.basicConfig(level=logging.ERROR)
+    startup_logger = logging.getLogger(__name__)
+
     try:
         source = Source(
             data_fetcher_model=TemplateDataFetcher,
@@ -31,7 +35,11 @@ def main() -> None:
         )
         base_collector.start()
     except KeyboardInterrupt:
+        startup_logger.warning("Keyboard interruption, exiting with exit code 0...")
         os._exit(0)
     except Exception as err:
-        print(err)
+        startup_logger.error(
+            f"Exception caught in the main function: {type(err).__name__} - {err}"
+        )
+        startup_logger.warning("Exiting with exit code 1...")
         sys.exit(1)
