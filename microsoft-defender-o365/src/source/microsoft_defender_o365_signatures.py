3440828bd0e5707fb535bbc6e31b2151531b8009
guzmud <ferdinand.bellissime@filigran.io>
2026-07-28 16:56:57 +0200

    feat(signatures): adding relevant signatures to supported (#499)

--- microsoft-defender-o365/src/source/microsoft_defender_o365_signatures.py
+++ microsoft-defender-o365/src/source/microsoft_defender_o365_signatures.py
@@ -1,5 +1,12 @@
 from pyoaev.signatures.types import SignatureTypes  # noqa: F401
 
-# No signature definitions in this chunk (#471 - project architecture setup).
-# Signatures will be added once the actual data collection logic is implemented.
-SUPPORTED_SIGNATURES: list[SignatureTypes] = []
+
+SUPPORTED_SIGNATURES: list[SignatureTypes] = [
+    SignatureTypes.SIG_TYPE_START_DATE,
+    SignatureTypes.SIG_TYPE_END_DATE,
+    SignatureTypes.SIG_TYPE_PARENT_PROCESS_NAME,
+    SignatureTypes.SIG_TYPE_URL_HASH,
+    SignatureTypes.SIG_TYPE_FILE_HASH,
+    SignatureTypes.SIG_TYPE_SOURCE_EMAIL,
+    SignatureTypes.SIG_TYPE_TARGET_EMAIL,
+]
