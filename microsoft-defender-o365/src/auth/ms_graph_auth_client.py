2b1b245f2b134a9fec4ff03cec085dd5f6382ef3
guzmud <ferdinand.bellissime@filigran.io>
2026-07-28 12:29:44 +0200

    feat(microsoft-defender-o365): kickstarting MSGraphAuthClient (#497)

---
+++ microsoft-defender-o365/src/auth/ms_graph_auth_client.py
@@ -0,0 +1,10 @@
+from src.models.settings.source_configs import _ConfigLoaderSource
+
+
+class MSGraphAuthClient:
+    def __init__(self, config: _ConfigLoaderSource) -> None:
+        pass
+
+    def get_access_token(self) -> str:
+        """Returns a valid bearer token string."""
+        pass
