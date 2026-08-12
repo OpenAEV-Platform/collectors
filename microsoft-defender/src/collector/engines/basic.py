1ffa14610858b72fcc6a2685630e95f01927d8e4
guzmud <ferdinand.bellissime@filigran.io>
2026-08-12 15:07:24 +0200

    fix(microsoft-defender): split engine step (4) in two: (4) produce alert data and (5) match it with injected signatures (#541)
    - introducing the AlertData type alias

--- microsoft-defender/src/collector/engines/basic.py
+++ microsoft-defender/src/collector/engines/basic.py
@@ -160,10 +160,11 @@ class BasicCollectorEngine:
         1. fetch the data using the data fetcher provided
         2. serialize each data as OAEVData
         3. group the expectation signatures per expectation
-        4. check for a match between the grouped signatures (3.) with the OAEVData (2.)
-        5. serialize each data as TraceData
-        6. check for a match between the expectation (0.) and the data (1.)
-        7. create the ExpectationResult using the previous match (6.) and the TraceData (5.)
+        4. transform the OAEVData into alert data usign signature type matching metadata
+        5. check for a match between the grouped signatures (3.) with the alert data (4.)
+        6. serialize each data as TraceData
+        7. check for a match between the expectation (0.) and the data (1.)
+        8. create the ExpectationResult using the previous match (7.) and the TraceData (6.)
         then return a list of all the results produced from the batch
         """
         batch_results = []
@@ -214,22 +215,27 @@ class BasicCollectorEngine:
                         )
                     )
 
-                    # (4) match expectation signature (3) with oaevdata (2) according to signatures
-                    flag = self.source_handler.match_signature_groups_and_oaevdata(
-                        signature_groups,
-                        oaev_data,
-                        self.oaev_detection_helper,
+                    # (4) transform the oaev data into alert data
+                    alert_data = self.source_handler.get_alert_data_from_oaev_data(
                         self.signatures,
+                        oaev_data,
+                    )
+
+                    # (5) match expectation signatures (3) with alert data (4)
+                    flag = self.source_handler.match_signature_groups_and_alert_data(
+                        signature_groups,
+                        alert_data,
+                        self.oaev_detection_helper,
                     )
                     if flag:
                         self.logger.info(
                             f"{LOG_PREFIX} Match for expectation {expectation.inject_expectation_id}"
                         )
-                        # (5) serialize data as tracedata
+                        # (6) serialize data as tracedata
                         trace = self.source_handler.serialize_as_tracedata(element)
                         traces.append(trace.model_dump())
 
-                        # (6) match expectation (0) with sourcedata (1)
+                        # (7) match expectation (0) with sourcedata (1)
                         matchflag, breakflag = (
                             self.source_handler.match_expectation_and_sourcedata(
                                 expectation, element
@@ -240,7 +246,7 @@ class BasicCollectorEngine:
                         if breakflag:
                             break
 
-                # (7) create results from step 6 + tracedata (5)
+                # (8) create results from step 7 + tracedata (6)
                 result = ExpectationResult(
                     expectation_id=str(expectation.inject_expectation_id),
                     is_valid=matched,
