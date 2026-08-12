45aa82a9bf3d9303077d31217fe3a8352c26cb67
Frédéric Basler <frederic.basler@filigran.io>
2026-08-12 07:51:45 +0200

    fix(crowdstrike): update linter

--- crowdstrike/tests/query_strategy/test_alert.py
+++ crowdstrike/tests/query_strategy/test_alert.py
@@ -1,5 +1,11 @@
 import unittest
 from datetime import datetime
+from unittest.mock import patch
+
+from pydantic import ValidationError
+from pyoaev.exceptions import OpenAEVError
+from pyoaev.signatures.signature_type import SignatureType
+from pyoaev.signatures.types import MatchTypes, SignatureTypes
 from tests.fixtures.crowdstrike_alerts_v2 import (
     ALERT_DATA,
     GET_ALERTS_V2_FAILURE_RESPONSE,
@@ -10,12 +16,6 @@ from tests.fixtures.crowdstrike_alerts_v2 import (
     QUERY_ALERTS_V2_SUCCESS_RESPONSE,
 )
 from tests.fixtures.defaults import DEFAULT_SIGNATURE_TYPES, get_default_api_handler
-from unittest.mock import patch
-
-from pydantic import ValidationError
-from pyoaev.exceptions import OpenAEVError
-from pyoaev.signatures.signature_type import SignatureType
-from pyoaev.signatures.types import MatchTypes, SignatureTypes
 
 from crowdstrike.query_strategy.alert import Alert, Item
 
