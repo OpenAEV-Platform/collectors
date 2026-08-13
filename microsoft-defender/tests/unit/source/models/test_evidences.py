06bda910956e1dff3dd0bdca7183d07db31832d4
guzmud <ferdinand.bellissime@filigran.io>
2026-08-13 12:42:33 +0200

    test(microsoft-defender): adding evidence unittests + better folder structure (#541)

---
+++ microsoft-defender/tests/unit/source/models/test_evidences.py
@@ -0,0 +1,161 @@
+import unittest
+
+import src.source.models.evidences as module
+
+
+class TestEvidence(unittest.TestCase):
+    def test_process_evidence(self):
+        process_data = {
+            "@odata.type": "#microsoft.graph.security.processEvidence",
+            "imageFile": {"fileName": "deadbeef.exe"},
+            "parentProcessImageFile": {"fileName": "badc0ffee.exe"},
+        }
+
+        process_evidence = module.Evidence(**process_data)
+
+        self.assertIsInstance(process_evidence, module.processEvidence)
+        self.assertEqual(
+            process_data.odata_type, "#microsoft.graph.security.processEvidence"
+        )
+        self.assertEqual(process_data.image_file.file_name, "deadbeef.exe")
+        self.assertEqual(
+            process_data.parent_process_image_file.file_name, "badc0ffee.exe"
+        )
+        self.assetIsNone(process_data.process_command_line)
+
+        extracted_evidences = process_evidence.extract_evidences()
+
+        self.assertIn(module.SignatureTypes.SIG_TYPE_PROCESS_NAME, extracted_evidences)
+        self.assertEqual(
+            extracted_evidences[module.SignatureTypes.SIG_TYPE_PROCESS_NAME],
+            ["deadbeef.exe", "badc0ffee.exe"],
+        )
+        self.assertIn(
+            module.SignatureTypes.SIG_TYPE_PARENT_PROCESS_NAME, extracted_evidences
+        )
+        self.assertEqual(
+            extracted_evidences[module.SignatureTypes.SIG_TYPE_PARENT_PROCESS_NAME],
+            ["deadbeef.exe", "badc0ffee.exe"],
+        )
+        self.assertNotIn(
+            module.SignatureTypes.SIG_TYPE_COMMAND_LINE, extracted_evidences
+        )
+
+    def test_device_evidence(self):
+        device_data = {
+            "@odata.type": "#microsoft.graph.security.deviceEvidence",
+            "deviceDnsName": "me.here",
+            "hostName": "me",
+            "lastIpAddress": "1.2.3.4",
+        }
+
+        device_evidence = module.Evidence(**device_data)
+
+        self.assertIsInstance(device_evidence, module.deviceEvidence)
+        self.assertEqual(
+            device_evidence.odata_type, "#microsoft.graph.security.deviceEvidence"
+        )
+        self.assertEqual(device_evidence.device_dns_name, "me.here")
+        self.assertEqual(device_evidence.host_name, "me")
+        self.assertEqual(device_evidence.last_ip_address, "1.2.3.4")
+        self.assertIsNone(device_evidence.last_external_ip_address)
+
+        extracted_evidences = device_evidence.extract_evidences()
+
+        self.assertIn(
+            module.SignatureTypes.SIG_TYPE_TARGET_HOSTNAME_ADDRESS, extracted_evidences
+        )
+        self.assertEqual(
+            extracted_evidences[module.SignatureTypes.SIG_TYPE_TARGET_HOSTNAME_ADDRESS],
+            ["me.here", "me"],
+        )
+        self.assertIn(
+            module.SignatureTypes.SIG_TYPE_TARGET_IPV4_ADDRESS, extracted_evidences
+        )
+        self.assertEqual(
+            extracted_evidences[module.SignatureTypes.SIG_TYPE_TARGET_IPV4_ADDRESS],
+            ["1.2.3.4"],
+        )
+
+    def test_file_evidence(self):
+        file_data = {
+            "@odata.type": "#microsoft.graph.security.fileEvidence",
+            "fileDetails": {
+                "fileName": "myfile.exe",
+                "filePath": "/this/is/myfile.exe",
+            },
+        }
+        file_evidence = module.Evidence(**file_data)
+
+        self.assertIsInstance(file_evidence, module.fileEvidence)
+        self.assertEqual(
+            file_evidence.odata_type, "#microsoft.graph.security.fileEvidence"
+        )
+        self.assertEqual(file_evidence.file_details.file_name, "myfile.exe")
+        self.assertEqual(file_evidence.file_details.file_path, "/this/is/myfile.exe")
+
+        extracted_evidences = file_evidence.extract_evidences()
+
+        self.assertIn(module.SignatureTypes.SIG_TYPE_FILE_NAME, extracted_evidences)
+        self.assertEqual(
+            extracted_evidences[module.SignatureTypes.SIG_TYPE_FILE_NAME],
+            ["myfile.exe", "/this/is/myfile.exe"],
+        )
+
+    def test_ip_evidence(self):
+        ip_data = {
+            "@odata.type": "#microsoft.graph.security.ipEvidence",
+            "ipAddress": "1.2.3.4",
+        }
+        ip_evidence = module.Evidence(**ip_data)
+
+        self.assertIsInstance(ip_evidence, module.ipEvidence)
+        self.assertEqual(ip_evidence.odata_type, "#microsoft.graph.security.ipEvidence")
+        self.assertEqual(ip_evidence.ip_address, "1.2.3.4")
+
+        extracted_evidences = ip_evidence.extract_evidences()
+
+        self.assertIn(
+            module.SignatureTypes.SIG_TYPE_TARGET_IPV4_ADDRESS, extracted_evidences
+        )
+        self.assertEqual(
+            extracted_evidences[module.SignatureTypes.SIG_TYPE_TARGET_IPV4_ADDRESS],
+            ["1.2.3.4"],
+        )
+
+    def test_generic_evidence(self):
+        generic_data = {
+            "@odata.type": "#microsoft.graph.security.userEvidence",
+            "key": "value",
+        }
+        generic_evidence = module.Evidence(**generic_data)
+
+        self.assertIsInstance(generic_evidence, module.genericEvidence)
+        self.assertEqual(
+            generic_evidence.odata_type, "#microsoft.graph.security.userEvidence"
+        )
+        self.assertEqual(generic_evidence.extract_evidences(), {})
+
+    def test_evidence_discriminator(self):
+        supported = [
+            "#microsoft.graph.security.fileEvidence",
+            "#microsoft.graph.security.deviceEvidence",
+            "#microsoft.graph.security.processEvidence",
+            "#microsoft.graph.security.ipEvidence",
+        ]
+        for odata_type in supported:
+            self.assertEqual(
+                odata_type,
+                module.discriminate_evidence_type({"@odata.type": odata_type}),
+            )
+
+        unsupported = [
+            "#microsoft.graph.security.userEvidence",
+            "#microsoft.graph.notsecurity.somethingelse",
+            "foobar",
+        ]
+        for odata_type in unsupported:
+            self.assertEqual(
+                "generic",
+                module.discriminate_evidence_type({"@odata.type": odata_type}),
+            )
