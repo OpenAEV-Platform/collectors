9c1861fa0055e1562b873e8e6e3703b3fedbd2b1
guzmud <ferdinand.bellissime@filigran.io>
2026-09-09 15:28:41 +0200

    chore(microsoft-defender): adding even more debug data (#541)

--- microsoft-defender/src/collector/engines/basic.py
+++ microsoft-defender/src/collector/engines/basic.py
@@ -310,6 +310,10 @@ class BasicCollectorEngine:
             self.logger.info(
                 f"{LOG_PREFIX} Processing a batch of expectations of size {len(expectations)}"
             )
+            for expectation in expectations:
+                self.logger.debug(
+                    f"{LOG_PREFIX} {expectation}"
+                )
             results.extend(self._process_batch(expectations))
 
             self.current_summary.processed = len(results)
