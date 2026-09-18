19fcfb2bffac1c3926971af7973bee4c22f9b246
guzmud <ferdinand.bellissime@filigran.io>
2026-09-18 15:54:10 +0200

    fix(microsoft-defender): custom matching engine (#541)
    plus splitting unittests for source handler and data fetcher

--- microsoft-defender/src/source/source_handler.py
+++ microsoft-defender/src/source/source_handler.py
@@ -1,9 +1,10 @@
 from datetime import UTC, datetime
 
+from pyoaev.helpers import OpenAEVDetectionHelper
 from pyoaev.signatures.types import SignatureTypes
 from src.collector.models.source import SourceHandler
 from src.collector.protocols.data_fetcher import FetchParamsHook
-from src.collector.types.collector import ExpectationsList
+from src.collector.types.collector import AlertData, ExpectationsList, SignatureGroups
 
 
 class DefenderSourceHandler(SourceHandler):
@@ -39,3 +40,41 @@ class DefenderSourceHandler(SourceHandler):
             return params
 
         return _hook
+
+    @staticmethod
+    def match_signature_groups_and_alert_data(
+        signature_groups: SignatureGroups,
+        alert_data: AlertData,
+        oaev_detection_helper: OpenAEVDetectionHelper,
+    ) -> bool:
+        """
+        Weighted matching system:
+        - look for any match per signature type (between multiple values on both side)
+        - update a global score according to the weight given to a signature type
+        Default weight is 1/3, threshold for matching is 1,
+        the size of the intersection does not influence the scoring
+        (a.k.a. two match on SIG_TYPE_TARGET_IPV4_ADDRESS is the same value as a single match)
+        """
+        weights = {
+            SignatureTypes.SIG_TYPE_PARENT_PROCESS_NAME: 1,
+            SignatureTypes.SIG_TYPE_PROCESS_NAME: 2 / 3,
+            SignatureTypes.SIG_TYPE_COMMAND_LINE: 2 / 3,
+            SignatureTypes.SIG_TYPE_FILE_NAME: 2 / 3,
+        }
+
+        score = 0
+
+        for key in signature_groups:
+            if key in alert_data:
+                signature_values = [
+                    sig.get("value")
+                    for sig in signature_groups[key]
+                    if sig.get("value")
+                ]
+                alert_values = alert_data[key].get("data", [])
+                intersection = set(signature_values) & set(alert_values)
+
+                if intersection:
+                    score += weights.get(key, 1 / 3)
+
+        return score >= 0.99
